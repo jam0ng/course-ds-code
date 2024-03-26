1f1a4f6e7e2057b20818b07c008bf4092f8d2150
jam0ng <87260699+jam0ng@users.noreply.github.com>
2024-03-26 12:22:13 +0900

    Add files via upload

---
+++ week2/1.assignment5.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+	int num = 0; // 원반 개수
+	scanf("%d", &num);
+	hanoi(num, 'A', 'B', 'C');
+	getchar();
+}
+
+int hanoi(int n, char start, char work, char target)
+{
+	if (n == 1) {
+		printf("\n%c에서 원반 %d를(을) %c로 옮김", start, n, target);
+		return 0;
+	}
+	hanoi(n - 1, start, target, work);
+	printf("\n%c에서 원반 %d를(을) %c로 옮김", start, n, target);
+	hanoi(n - 1, work, start, target);
+	
+	return 0;
+
+}
